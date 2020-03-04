10aa6c3c034c994435aa49463892bf23382e3abf
junhg0211 <junhg0211@gmail.com>
2020-03-04 18:32:50 +0900

    Information

--- index.js
+++ index.js
@@ -33,3 +33,13 @@ document.addEventListener("wheel", (event) => {
 window.addEventListener('scroll', tick);
 
 window.setInterval(tick, frameDurationInMillis);
+
+function calculate() {
+    var duration = parseFloat(document.getElementById("duration").value);
+    var tracks = parseFloat(document.getElementById("tracks").value);
+    var deadline = parseFloat(document.getElementById("deadline").value);
+
+    var result = parseInt(5 / Math.sqrt(deadline) * tracks * duration * 2.5) * 1000;
+
+    document.getElementById("result").innerHTML = `${duration}분, ${tracks}트랙, ${deadline}일 마감 :: ${result} KRW`;
+}
