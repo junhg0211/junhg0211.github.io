501ac6578c387c606564694d5f8bee18d4497c50
junhg0211 <junhg0211@gmail.com>
2020-03-04 18:54:15 +0900

    Sample

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ document.addEventListener("wheel", (event) => {
     // window.scrollTo(0, 0);
 });
 
+Number.prototype.format = function(){
+    if (this == 0) return 0;
+ 
+    var reg = /(^[+-]?\d+)(\d{3})/;
+    var n = (this + '');
+ 
+    while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
+ 
+    return n;
+};
+
 window.addEventListener('scroll', tick);
 
 window.setInterval(tick, frameDurationInMillis);
@@ -41,5 +52,5 @@ function calculate() {
 
     var result = parseInt(5 / Math.sqrt(deadline) * tracks * duration * 2.5) * 1000;
 
-    document.getElementById("result").innerHTML = `${duration}분, ${tracks}트랙, ${deadline}일 마감 :: ${result} KRW`;
+    document.getElementById("result").innerHTML = `${duration}분, ${tracks}트랙, ${deadline}일 마감 :: ${result.format()} KRW`;
 }
