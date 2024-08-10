b36cf195e499baf2cc345b1ce6859a86aec2d19a
Sch <junhg0211@gmail.com>
2024-08-11 00:29:14 +0900

    button transition animtion in social box

--- src/SocialBox.svelte
+++ src/SocialBox.svelte
@@ -1,14 +1,43 @@
 <script>
+  import { onMount } from 'svelte';
+
 	export let src;
 	export let alt;
 	export let href="#";
 	export let name;
 	export let profile;
+
+  let image;
+  let rect;
+
+  function enlarge() {
+    rect = image.getBoundingClientRect();
+    image.style.transition = "0.2s ease-in-out box-shadow";
+  }
+
+  function unenlarge() {
+    image.style.transform = "";
+    image.style.transition = "0.2s ease-in-out all";
+  }
+
+  function mousemove(e) {
+    let dx = (e.clientX - (rect.x + rect.width / 2)) / 5;
+    let dy = (e.clientY - (rect.y + rect.height / 2)) / 5;
+    image.style.transform = `translate(${dx}px, ${dy}px)`;
+  }
 </script>
 
 <div class="social-box">
   <a {href} target={href === "#" ? "" : "_blank"}>
-		<img {src} {alt} class="social-logo" />
+		<img
+      {src}
+      {alt}
+      class="social-logo"
+      on:mouseenter={enlarge}
+      on:mouseleave={unenlarge}
+      on:mousemove={mousemove}
+      bind:this={image}
+    />
 	</a>
 	<p class="social-name">{name}</p>
 	{#if profile}
