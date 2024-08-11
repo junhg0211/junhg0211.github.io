8344ab14b9500554d9d1507363c45f95e93e9ff1
Sch <junhg0211@gmail.com>
2024-08-11 19:31:07 +0900

    change title page'

--- src/TitlePage.svelte
+++ src/TitlePage.svelte
@@ -2,7 +2,6 @@
   let nameDecoration;
   let angle = 0;
 
-  /*
   setInterval(() => {
     angle++;
     angle %= 360;
@@ -10,25 +9,22 @@
     if (nameDecoration == undefined || nameDecoration == null)
       return;
 
-    nameDecoration.style.background = `linear-gradient(${angle}deg, #ffcf00 20%, #6bd94a 80%, #00cfcf 100%)`;
+    nameDecoration.style.background = `linear-gradient(${angle}deg, #ffce00 0%, #19191e 100%)`;
     nameDecoration.style.backgroundClip = `text`;
   }, 1000/24);
-  */
 </script>
 
-<img src="./logo/sch_logo2_lettering.svg" alt="스치 로고" style="height: 7rem" />
+<img src="./logo/logo_mixed.svg" alt="스치 로고" style="height: 7rem" />
 <h1>Hangyeol <span class="name-decoration" bind:this={nameDecoration}>“Sch”</span> Jeon</h1>
 <p>지식을 갈구하다.</p>
 
 <style>
 	.name-decoration {
-		color: #ffcf00;
-    /*
-		background: linear-gradient(90deg, #ffcf00 10%, #6bd94a 70%, #00cfcf 100%);
+		/* color: #ffce00; */
+		background: linear-gradient(0deg, #ffce00 00%, #19191e 100%);
 		background-clip: text;
 		-webkit-background-clip: text;
 		-webkit-text-fill-color: transparent;
-    */
 	}
 </style>
 
