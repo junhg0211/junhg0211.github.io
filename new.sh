f854068cb8e7ee76b49948b9e94ac780f1123a76
Sch <junhg0211@gmail.com>
2026-09-17 15:12:59 +0900

    Change new.sh

--- new.sh
+++ new.sh
@@ -5,6 +5,18 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-FILE="content/$(date +%Y/%m)/$1.md"
+TITLE="$*"
+SLUG=$(printf '%s' "$TITLE" | tr ' ' '-')
+FILE="content/$(date +%Y/%m)/$SLUG.md"
 
-hugo new "$FILE" && nvim "$FILE"
+if command -v nvim >/dev/null 2>&1; then
+    EDITOR="nvim"
+else
+    EDITOR="vim"
+fi
+
+if [ ! -f "$FILE" ]; then
+    hugo new "$FILE" || exit 1
+fi
+
+"$EDITOR" "$FILE"
